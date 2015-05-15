8104d5be11b676436b02a71c472698e22dbc625b
Zou Jian <chsword@126.com>
2015-05-15 11:51:00 +0800

    工

---
+++ Chsword.XamarinAndroid.OnlineUpdate/Class1.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chsword.XamarinAndroid.OnlineUpdate
+{
+    public class Class1
+    {
+    }
+}
